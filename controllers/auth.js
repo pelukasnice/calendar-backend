5cf6be8a315fee09df713ac2cfa1dc52989f68dc
pelukasnice <103325490+pelukasnice@users.noreply.github.com>
2025-05-30 19:49:20 -0300

    express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,22 @@
 const { response } = require('express')
-
-
+const {validationResult} = require('express-validator')
 
 const crearUsuario = (req, res = response) => {    
     
     const {name, email, password} = req.body;
 
-    res.json({
+    //manejo de errores
+    const errors = validationResult( req );
+
+    if ( !errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+    //console.log(errors)
+
+    res.status(201).json({
         ok: true,
         msg:'registro',
         name,
@@ -19,7 +29,16 @@ const loginUsuario = (req, res = response) => {
     
     const { email, password } = req.body;
 
-    res.json({
+    const errors = validationResult( req );
+
+    if ( !errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
+    res.status(201).json({
         ok: true,
         msg:'login',
         email,
