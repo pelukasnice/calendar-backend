43491f4717ea99191f3ce0ae8518408a637ff347
pelukasnice <103325490+pelukasnice@users.noreply.github.com>
2025-05-31 18:56:12 -0300

    crear usuario en db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,36 +1,48 @@
-const { response } = require('express')
+const { response } = require('express');
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response) => {    
-    
-    const {name, email, password} = req.body;
+const crearUsuario = async (req, res = response) => {
 
-    res.status(201).json({
-        ok: true,
-        msg:'registro',
-        name,
-        email,
-        password
-    })
+    //const {name, email, password} = req.body;
+
+    try {
+
+        const usuario = new Usuario(req.body)
+
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status.json({
+            ok: false,
+            msg: 'Por favor hable con el admin'
+        })
+    }
 }
 
-const loginUsuario = (req, res = response) => {  
-    
+const loginUsuario = (req, res = response) => {
+
     const { email, password } = req.body;
 
     res.status(201).json({
         ok: true,
-        msg:'login',
+        msg: 'login',
         email,
         password
     })
 }
 
 
-const revalidarToken = (req, res = response) => {    
+const revalidarToken = (req, res = response) => {
 
     res.json({
         ok: true,
-        msg:'renew'
+        msg: 'renew'
     })
 }
 
